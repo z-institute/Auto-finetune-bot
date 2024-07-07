0e870b3d60c060dccfdffd83baf660f029bb3557
johnson70630 <johnson70630@gmail.com>
2024-07-07 20:22:31 +0800

    Update line_bot.py

--- line_bot.py
+++ line_bot.py
@@ -37,6 +37,7 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
@@ -47,41 +48,46 @@ def handle_message(event):
         user_states[user_id] = "start"
         user_messages[user_id] = []
 
-    # does not do lower when input is api key
-    if user_states[user_id] == "waiting_for_api":
-        received_text = event.message.text
-    else:
-        received_text = event.message.text.strip()
+    received_text = event.message.text if user_states[user_id] == "waiting_for_api" else event.message.text.strip()
+    response_text, quick_reply = process_user_message(user_id, received_text)
+
+    # Send reply with quick replies if available
+    text_message = TextSendMessage(text=response_text, quick_reply=quick_reply) if quick_reply else TextSendMessage(text=response_text)
+    line_bot_api.reply_message(event.reply_token, text_message)
+
+
+def process_user_message(user_id, received_text):
+    state = user_states[user_id]
 
     # Respond based on user state
-    if user_states[user_id] == "start":
+    if state == "start":
         response_text = "請點擊「描述模型」來輸入你對想要訓練的模型的描述"
         quick_reply = create_quick_replies(["描述模型"])
         user_states[user_id] = "waiting_for_instruction"
-    elif received_text == "描述模型" and user_states[user_id] == "waiting_for_instruction":
+    elif received_text == "描述模型" and state == "waiting_for_instruction":
         response_text = "請輸入你對想要訓練的模型的描述"
         quick_reply = None
         user_states[user_id] = "instruction_complete"
-    elif user_states[user_id] == "instruction_complete":
+    elif state == "instruction_complete":
         user_messages[user_id].append(received_text)
         response_text = "點擊「更新對話資料」來輸入模型的訓練資料，或選擇「刪除資料」來重置訓練資料集！"
         quick_reply = create_quick_replies(["更新對話資料", "刪除資料"])
-        user_states[user_id] = "waiting_for_train_model"
-    elif received_text == "刪除資料" and user_states[user_id] == "waiting_for_train_model":
+        user_states[user_id] = "input_data"
+    elif received_text == "刪除資料" and state == "input_data":
         response_text = "確定要刪除資料嗎？"
         quick_reply = create_quick_replies(["是", "否"])
         user_states[user_id] = "delete_or_not"
-    elif received_text == "是" and user_states[user_id] == "delete_or_not":
+    elif received_text == "是" and state == "delete_or_not":
         delete_data(user_id)
         response_text = "已刪除資料！請點擊「描述模型」來重新輸入你對想要訓練的模型的描述"
         quick_reply = create_quick_replies(["描述模型"])
         user_states[user_id] = "waiting_for_instruction"
-    elif received_text == "否" and user_states[user_id] == "delete_or_not":
+    elif received_text == "否" and state == "delete_or_not":
         response_text = "刪除資料取消！點擊「更新對話資料」來輸入模型的訓練資料"
         quick_reply = create_quick_replies(["更新對話資料"])
-        user_states[user_id] = "waiting_for_train_model"
+        user_states[user_id] = "input_data"
     # wait for fisrt input
-    elif received_text == "更新對話資料" and user_states[user_id] == "waiting_for_train_model":
+    elif received_text == "更新對話資料" and state == "input_data":
         response_text = "請輸入對話："
         quick_reply = None
         user_states[user_id] = "waiting_for_first_input"
@@ -98,73 +104,62 @@ def handle_message(event):
         quick_reply = create_quick_replies(["輸入另一筆對話資料", "停止輸入並儲存資料"])
         user_states[user_id] = "waiting_for_continue_or_stop"
     # keep input
-    elif received_text == "輸入另一筆對話資料" and user_states[user_id] == "waiting_for_continue_or_stop":
+    elif received_text == "輸入另一筆對話資料" and state == "waiting_for_continue_or_stop":
         response_text = "請輸入對話:"
         quick_reply = None
         user_states[user_id] = "waiting_for_first_input"
     # stop input and save data
-    elif received_text == "停止輸入並儲存資料" and user_states[user_id] == "waiting_for_continue_or_stop":
+    elif received_text == "停止輸入並儲存資料" and state == "waiting_for_continue_or_stop":
         #  Process the user_messages[user_id] dictionary as needed for training the model
         save_to_json(user_id)
-        enough_data = check_data(user_id)
-        if enough_data:
+        if check_data(user_id):
             response_text = "資料儲存完成！已達10筆，是否訓練模型？"
             quick_reply = create_quick_replies(["是", "否"])
             user_states[user_id] = "train_or_not"
         else:
             response_text = "好的，資料已儲存（10筆才能訓練模型），已輸入過的就不用再輸入一次囉！點擊「更新對話資料」來繼續輸入模型的訓練資料，或選擇「刪除資料」來重置訓練資料集～"
             quick_reply = create_quick_replies(["更新對話資料", "刪除資料"])
-            user_states[user_id] = "waiting_for_train_model"
+            user_states[user_id] = "input_data"
     # train model and get api
-    elif received_text == "是" and user_states[user_id] == "train_or_not":
-        response_text = "請輸入你的 openai api key~ 備註：在訓練時請勿輸入任何文字，否則訓練會中斷！請耐心等待模型訓練完成的回覆～"
+    elif received_text == "是" and state == "train_or_not":
+        response_text = "請輸入你的 openai api key~ 備註：每一分鐘會回覆 fine tune 狀態，請耐心等待模型訓練完成的回覆～"
         quick_reply = None
         user_states[user_id] = "waiting_for_api"
     # train model
-    elif user_states[user_id] == "waiting_for_api":
+    elif state == "waiting_for_api":
         api = received_text
         result = fine_tuning(user_id, api)
         if result == 'success':
             response_text = "模型訓練完成! 點擊「更新對話資料」來繼續輸入模型的訓練資料，或選擇「刪除資料」來重置訓練資料集！"
             quick_reply = create_quick_replies(["更新對話資料", "刪除資料"])
-            user_states[user_id] = "waiting_for_train_model"
+            user_states[user_id] = "input_data"
         else:
             response_text = "api 錯誤! 請重新輸入 api"
             quick_reply = None
     # not to train model and back to the original status
-    elif received_text == "否" and user_states[user_id] == "train_or_not":
-        response_text = "好的，資料已儲存，以輸入過的就不用再輸入一次囉！點擊「更新對話資料」來繼續輸入模型的訓練資料，或選擇「刪除資料」來重置訓練資料集～"
+    elif received_text == "否" and state == "train_or_not":
+        response_text = "好的，資料已儲存，已輸入過的就不用再輸入一次囉！點擊「更新對話資料」來繼續輸入模型的訓練資料，或選擇「刪除資料」來重置訓練資料集～"
         quick_reply = create_quick_replies(["更新對話資料", "刪除資料"])
-        user_states[user_id] = "waiting_for_train_model"
+        user_states[user_id] = "input_data"
     # to avoid illegel input when not collecting data
     else:
         response_text = "不支援此功能, 點擊「更新對話資料」來輸入模型的訓練資料，或選擇「刪除資料」來重置訓練資料集！"
         quick_reply = create_quick_replies(["更新對話資料", "刪除資料"])
-        user_states[user_id] = "waiting_for_train_model"
-
-
-    # Send reply with quick replies if available
-    text_message = TextSendMessage(text=response_text, quick_reply=quick_reply) if quick_reply else TextSendMessage(text=response_text)
-    line_bot_api.reply_message(event.reply_token, text_message)
-
+        user_states[user_id] = "input_data"
+    return response_text, quick_reply
 
 
 # Create quick reply buttons
 def create_quick_replies(options):
-    quick_reply_items = [
-        QuickReplyButton(action=MessageAction(label=option, text=option.lower()))
-        for option in options
-    ]
-    return QuickReply(items=quick_reply_items)
+    return QuickReply(items=[QuickReplyButton(action=MessageAction(label=option, text=option)) for option in options])
+
     
 def delete_data(user_id):
-    user_messages[user_id] = []
-    file_path = f"{user_id}_data.json"
+    try:
+        os.remove(f"{user_id}_data.json")
+    except FileNotFoundError:
+        pass
 
-    # Check if file exists
-    if os.path.exists(file_path):
-        # Read existing data
-        os.remove(file_path)
 
 # Save user messages to JSON file
 def save_to_json(user_id):
@@ -204,14 +199,14 @@ def save_to_json(user_id):
     user_messages[user_id] = [system_message]
     print(f" Data extraction completed. JSONL file saved to: {output_file}")
 
+
 def check_data(user_id):
     data_path = f'{user_id}_data.json'
     # Load the dataset
     with open(data_path, 'r', encoding='utf-8') as f:
         dataset = [json.loads(line) for line in f]
-    if len(dataset) >= 10:
-        return True
-    return False
+    return len(dataset) >= 10
+
 
 def fine_tuning(user_id, api):
     client = OpenAI(api_key=api)
@@ -231,18 +226,23 @@ def fine_tuning(user_id, api):
         )
     fine_tune_id = response.id
     print(f"Fine-tuning started, fine-tune ID: {fine_tune_id}")
+    # Send initial status to user
+    line_bot_api.push_message(user_id, TextSendMessage(text=f"開始 Fine-tune! Fine-tune ID:\n{fine_tune_id}"))
 
     # Monitor fine-tuning status
     while True:
         response = client.fine_tuning.jobs.retrieve(fine_tuning_job_id=fine_tune_id)
         status = response.status
         print(f"Fine-tuning status: {status}")
+        # Send status update to user
+        status_message = f"目前 Fine-tune 狀態: {status}"
+        line_bot_api.push_message(user_id, TextSendMessage(text=status_message))
         if status == 'succeeded':
             return 'success'
         elif status == 'failed':
             print("Fine-tuning failed.")
             return 'failed'
-        time.sleep(60)  # Wait for 1 minute before checking the status agai
+        time.sleep(60)  # Wait for 1 minute before checking the status again
 
 
 if __name__ == "__main__":
